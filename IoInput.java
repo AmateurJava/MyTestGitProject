a10eb45ebebc608a403c792c3e28954b8992c6ad
AmateurJava <liangjyft@163.com>
2018-12-15 21:59:50 +0800

    修改java文件。

--- IoInput.java
+++ IoInput.java
@@ -1,1 +1,16 @@
-love do java
+package com.liang.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
